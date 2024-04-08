404a8d30fa31b2f954808c91c1bf1af4d81e7181
Iggy Meagher <ignatius.meagher@gmail.com>
2024-04-09 09:50:55 +1000

    did a for loop for the basic action for my project. Needs comments

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("testing if commit will work")
+import random
+
+
+arr = ['apple', 'pear', 'orange', 'broccoli']
+
+
+
+for question in range(20): #loops through 20 questions
+    rand = random.randint(0,3) #random number to be used in the array
+    rand_fv = arr[rand]
+    print("What fruit is ", rand_fv)
+    answer = input("")
+    if answer == rand_fv:
+        question = question +1
+    else:
+        break
